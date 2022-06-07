253c1c8da7a51740bc1f96701615849e97f9fd07
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-06-07 14:25:47 +0900

    feat: fe // myadmin - class registration

--- frontend/src/pages/admin/MyAdminClassRegistration.jsx
+++ frontend/src/pages/admin/MyAdminClassRegistration.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import Select from 'react-select';
+import Spinner from '../../components/shared/Spinner';
+
 import {
 	WrapperStyles,
 	FormStyles,
@@ -7,33 +10,78 @@ import {
 	InputStyles,
 	SubmitStyles,
 	ButtonStyles,
-} from './styles/MyAdminSignInStyles';
+} from './styles/MyAdminClassRegistrationStyles';
+
+const typeOptions = [
+	{ value: 'basicClass', label: 'Basic Class', name: 'type' },
+	{ value: 'advClass', label: 'Advanced Class', name: 'type' },
+	{ value: 'pdClass', label: 'PD Class', name: 'type' },
+];
+
+const statusOptions = [
+	{ value: 'open', label: 'Open', name: 'status' },
+	{ value: 'pending', label: 'Pending', name: 'status' },
+	{ value: 'closed', label: 'Closed', name: 'status' },
+];
+
+const weeksOptions = [
+	{ value: 4, label: '4주', name: 'weeks' },
+	{ value: 8, label: '8주', name: 'weeks' },
+];
+
+const priceOptions = [
+	{ value: 300000, label: 300000, name: 'price' },
+	{ value: 600000, label: 600000, name: 'price' },
+];
+
+const monthOptions = [
+	{ value: 1, label: '1월', name: 'month' },
+	{ value: 2, label: '2월', name: 'month' },
+	{ value: 3, label: '3월', name: 'month' },
+	{ value: 4, label: '4월', name: 'month' },
+	{ value: 5, label: '5월', name: 'month' },
+	{ value: 6, label: '6월', name: 'month' },
+	{ value: 7, label: '7월', name: 'month' },
+	{ value: 8, label: '8월', name: 'month' },
+	{ value: 9, label: '9월', name: 'month' },
+	{ value: 10, label: '10월', name: 'month' },
+	{ value: 11, label: '11월', name: 'month' },
+	{ value: 12, label: '12월', name: 'month' },
+];
 
 function MyAdminOpenClass() {
 	const [loading, setLoading] = useState(false);
-	const [isDisabled, setIsDisabled] = useState(true);
+	const [isDisabled, setIsDisabled] = useState(false);
 
 	const [formData, setFormData] = useState({
 		title: '',
-		type: '',
-		status: '',
-		month: null,
-		weeks: null,
+		type: 'basicClass',
+		status: 'pending',
+		month: 1,
+		weeks: 4,
 		period: '',
 		hours: '',
 		tutor: '',
-		price: null,
+		price: 300000,
 		isOnAir: false,
-		completedAt: null,
+		completedAt: -1,
 	});
 
 	const { title } = formData;
 
-	const handleSubmit = (params) => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+	};
 
 	const handleChange = ({ target: { id, value } }) => {
 		setFormData({ ...formData, [id]: value });
 	};
+
+	const handleSelectionChange = ({ name, value }) => {
+		setFormData({ ...formData, [name]: value });
+	};
+
 	return (
 		<WrapperStyles>
 			<FormStyles onSubmit={handleSubmit}>
@@ -45,14 +93,58 @@ function MyAdminOpenClass() {
 						id="title"
 						value={title}
 						onChange={handleChange}
-						required
+						// required
+					/>
+				</InputGroupStyles>
+
+				<InputGroupStyles>
+					<label htmlFor="type">강의 타입</label>
+					<Select
+						defaultValue={typeOptions[0]}
+						options={typeOptions}
+						onChange={handleSelectionChange}
+					/>
+				</InputGroupStyles>
+
+				<InputGroupStyles>
+					<label htmlFor="status">강의 상태</label>
+					<Select
+						defaultValue={statusOptions[1]}
+						options={statusOptions}
+						onChange={handleSelectionChange}
+					/>
+				</InputGroupStyles>
+
+				<InputGroupStyles>
+					<label htmlFor="month">강의 시작(month)</label>
+					<Select
+						defaultValue={monthOptions[0]}
+						options={monthOptions}
+						onChange={handleSelectionChange}
+					/>
+				</InputGroupStyles>
+
+				<InputGroupStyles>
+					<label htmlFor="weeks">강의 기간(weeks)</label>
+					<Select
+						defaultValue={weeksOptions[0]}
+						options={weeksOptions}
+						onChange={handleSelectionChange}
+					/>
+				</InputGroupStyles>
+
+				<InputGroupStyles>
+					<label htmlFor="price">강의 가격</label>
+					<Select
+						defaultValue={priceOptions[0]}
+						options={priceOptions}
+						onChange={handleSelectionChange}
 					/>
 				</InputGroupStyles>
 
 				<SubmitStyles>
 					<ButtonStyles isDisabled={isDisabled}>
-						<span>로그인</span>
-						<span tw="w-5"></span>
+						<span>개설하기</span>
 					</ButtonStyles>
 				</SubmitStyles>
 			</FormStyles>
---
+++ frontend/src/pages/admin/styles/MyAdminClassRegistrationStyles.js
@@ -0,0 +1,35 @@
+import tw, { styled } from 'twin.macro';
+
+export const WrapperStyles = styled.div`
+	${tw`grid place-content-center tracking-wider h-screen md:text-sm`}
+`;
+
+export const FormStyles = styled.form`
+	${tw`space-y-5 max-w-sm mx-auto bg-white py-8 px-4  min-w-[360px]`}
+
+	h2 {
+		${tw`text-center mb-16`}
+	}
+`;
+export const InputGroupStyles = styled.div`
+	${tw`flex flex-col space-y-2 `}
+`;
+
+export const InputStyles = styled.input`
+	${tw` text-base py-1 px-1.5 rounded`}
+	${tw`border-st_alt1 border-opacity-30 border-solid border-[1px]`}
+`;
+
+export const SubmitStyles = styled.div`
+	${tw`pt-5 space-y-7`}
+`;
+
+export const ButtonStyles = styled.button`
+	${tw`py-1.5 px-3 sm:py-2.5 sm:px-4 tracking-wider rounded-lg sm:hover:opacity-90`}
+	${tw`bg-[#ea4b4e] text-white `}
+	${tw`text-center w-full text-base`}
+
+	${({ isDisabled }) =>
+		isDisabled &&
+		tw`bg-st_bg1 text-gray-500 cursor-not-allowed sm:hover:opacity-100`}
+`;
