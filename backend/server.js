9fd3f7bf2dfed762e472bf94010f989e809444f6
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-09-14 16:39:19 +0700

    feat: prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 import express from 'express';
 import connetDB from './config/db.js';
 connetDB();
@@ -33,6 +34,21 @@ app.use('/api/order', orderRoutes);
 //* Works
 import worksRoutes from './Routes/workRoutes.js';
 app.use('/api/work', worksRoutes);
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	// FIX: below code fixes app crashing on refresh in deployment
+	app.get('*', (_, res) => {
+		res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to Storytunes' });
+	});
+}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
